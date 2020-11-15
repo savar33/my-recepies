da6dc112a34bfbe58296151b3f2980f5410f42c4
Misha Mas <savar33@gmail.com>
2020-11-15 12:34:42 +0200

    footer and navbar2 components

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FooterComponent() {
+    // return (
+    //     <>
+    //         Footer
+    //     </>
+    // );
+    
+    return 'Footer';
+}
+
+export default FooterComponent;
---
+++ src/components/NavbarComponent2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NavbarComponent2(props) {
+    return 'Navbar Component 2'
+}
+
+export default NavbarComponent2;
